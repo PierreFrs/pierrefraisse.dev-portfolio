24b04172c80211cc158882a2b280274e3e377e38
pfraisse <pierrefraisse90@gmail.com>
2024-12-10 11:59:38 +0100

    feat(projects): created slider

--- src/components/main_components/header.tsx
+++ src/components/main_components/header.tsx
@@ -3,9 +3,9 @@ import HeaderLink from "../headerLink";
 export default async function Header() {
     return (
         <header className="top-0 w-full flex justify-between items-center px-6 py-4 bg-transparent z-50">
-            <div className="text-2xl font-bold text-white">
+            <div className="text-xl font-bold uppercase">
                 <span>
-                    Logo
+                    Pierre Fraisse
                 </span>
             </div>
             <nav className="flex space-x-6">
