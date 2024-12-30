9250d9716e7f3281b8112c54e42bc669314a6c87
pfraisse <pierrefraisse90@gmail.com>
2024-12-30 13:41:36 +0100

    feat(logout): Changed button

--- src/components/admin_components/logoutButton.tsx
+++ src/components/admin_components/logoutButton.tsx
@@ -1,16 +1,17 @@
 import { PowerIcon } from "@heroicons/react/24/outline";
 import * as actions from "app/actions";
+import {Button} from "@nextui-org/button";
+import {Form} from "@nextui-org/form";
 
 export default function LogoutButton() {
     return (
-        <form action={actions.signOut}>
-            <button
-                className="flex items-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600"
+        <Form action={actions.signOut}>
+            <Button
                 type="submit"
             >
                 <PowerIcon className="w-6" />
                 <span>Sign Out</span>
-            </button>
-        </form>
+            </Button>
+        </Form>
     );
 }
