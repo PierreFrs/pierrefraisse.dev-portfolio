6fc35b42f2a2c404d940160aef0fcfcd9a45ae69
pfraisse <pierrefraisse90@gmail.com>
2025-01-06 15:26:18 +0100

    Consolidated migrations

--- prisma/migrations/20241210151140_add_user_id_language_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId,language]` on the table `HeroDescription` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "HeroDescription_userId_language_key" ON "HeroDescription"("userId", "language");
